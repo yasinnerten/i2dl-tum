f456101a5fce98a24d615bf40f3626f373da9664
yasinnerten <yasinnerten@hotmail.com>
2026-01-19 12:41:04 +0100

    exercise 9

--- exercise_09/exercise_code/networks/keypoint_nn.py
+++ exercise_09/exercise_code/networks/keypoint_nn.py
@@ -52,21 +52,21 @@ class KeypointModel(nn.Module):
             nn.MaxPool2d(2, 2),
             nn.Dropout2d(0.2),
             
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Dropout2d(0.2),
         )
         
         self.fc_layers = nn.Sequential(
-            nn.Linear(256 * 6 * 6, 1024),
+            nn.Linear(128 * 6 * 6, 512),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(1024, 512),
+            nn.Linear(512, 256),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(512, 30)
+            nn.Linear(256, 30)
         )
 
         ########################################################################
